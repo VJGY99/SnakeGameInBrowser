9517b30e0c7931ca49d4be15240520ad0bc8345c
VJGY99 <80971475+VJGY99@users.noreply.github.com>
2021-09-11 13:11:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,169 +1,196 @@
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
-//
-canvas.width = canvas.height = 600
-canvas.style.width = '900px'
-canvas.style.height = '900px'
-canvas.style.border = '3px solid #000000'
-//
-const CELL_SIZE = 30
-const WORLD_WIDTH = Math.floor(canvas.width / CELL_SIZE)
-const WORLD_HEIGHT = Math.floor(canvas.height / CELL_SIZE)
-const MOVE_INTERVAL = 300
-const FOOD_SPAWN_INTERVAL = 1500
-//
-let input
-let snake
-let foods
-let foodSpawnElapsed
-let gameOver
-let score
-//
-function reset () {
-  input = {}
-  snake = {
-    moveElapsed: 0,
-    length: 4,
-    parts: [{
-      x: 0,
-      y: 0
-    }],
-    dir: null,
-    newDir: {
-      x: 1,
-      y: 0
-    }
-  }
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
-  foods = [
-    {
-      x: 10,
-      y: 0
-    }
-  ]
-
-  foodSpawnElapsed = 0
-  gameOver = false
-  score = 0
-}
-
-function update (delta) {
-  if (gameOver) {
-    if (input[' ']) {
-      reset()
-    }
-
-    return
-  }
-//
-  if (input.ArrowLeft && snake.dir.x !== 1) {
-    snake.newDir = { x: -1, y: 0 }
-  } else if (input.ArrowUp && snake.dir.y !== 1) {
-    snake.newDir = { x: 0, y: -1 }
-  } else if (input.ArrowRight && snake.dir.x !== -1) {
-    snake.newDir = { x: 1, y: 0 }
-  } else if (input.ArrowDown && snake.dir.y !== -1) {
-    snake.newDir = { x: 0, y: 1 }
-  }
-
-  snake.moveElapsed += delta
-
-  if (snake.moveElapsed > MOVE_INTERVAL) {
-    snake.dir = snake.newDir
-    snake.moveElapsed -= MOVE_INTERVAL
-    const newSnakePart = {
-      x: snake.parts[0].x + snake.dir.x,
-      y: snake.parts[0].y + snake.dir.y
-    }
-    snake.parts.unshift(newSnakePart)
-
-    if (snake.parts.length > snake.length) {
-      snake.parts.pop()
-    }
-
-    const head = snake.parts[0]
-    const foodEatenIndex = foods.findIndex(f => f.x === head.x && f.y === head.y)
-    if (foodEatenIndex >= 0) {
-      snake.length++
-      score++
-      foods.splice(foodEatenIndex, 1)
-    }
-
-    const worldEdgeIntersect = head.x < 0 || head.x >= WORLD_WIDTH || head.y < 0 || head.y >= WORLD_HEIGHT
-    if (worldEdgeIntersect) {
-      gameOver = true
-      return
-    }
-
-    const snakePartIntersect = snake.parts.some((part, index) => index !== 0 && head.x === part.x && head.y === part.y)
-    if (snakePartIntersect) {
-      gameOver = true
-      return
-    }
-  }
-
-  foodSpawnElapsed += delta
-  if (foodSpawnElapsed > FOOD_SPAWN_INTERVAL) {
-    foodSpawnElapsed -= FOOD_SPAWN_INTERVAL
-    foods.push({
-      x: Math.floor(Math.random() * WORLD_WIDTH),
-      y: Math.floor(Math.random() * WORLD_HEIGHT)
-    })
+class SnakePart {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 }
 
-function render () {
-  ctx.textAlign = 'center'
-  ctx.textBaseline = 'middle'
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
+let speed = 7;
 
-  ctx.fillStyle = 'black'
-  snake.parts.forEach(({ x, y }, index) => {
-    if (index === 0) {
-      ctx.fillStyle = 'black'
-    } else {
-      ctx.fillStyle = '#555'
+let tileCount = 20;
+let tileSize = canvas.width / tileCount - 2;
+
+let headX = 10;
+let headY = 10;
+const snakeParts = [];
+let tailLength = 2;
+
+let appleX = 5;
+let appleY = 5;
+
+let inputsXVelocity = 0;
+let inputsYVelocity = 0;
+
+let xVelocity = 0;
+let yVelocity = 0;
+
+let score = 0;
+
+const gulpSound = new Audio("gulp.mp3");
+
+//game loop
+function drawGame() {
+  xVelocity = inputsXVelocity;
+  yVelocity = inputsYVelocity;
+
+  changeSnakePosition();
+  let result = isGameOver();
+  if (result) {
+    return;
+  }
+
+  clearScreen();
+
+  checkAppleCollision();
+  drawApple();
+  drawSnake();
+
+  drawScore();
+
+  if (score > 5) {
+    speed = 9;
+  }
+  if (score > 10) {
+    speed = 11;
+  }
+
+  setTimeout(drawGame, 1000 / speed);
+}
+
+function isGameOver() {
+  let gameOver = false;
+
+  if (yVelocity === 0 && xVelocity === 0) {
+    return false;
+  }
+
+  //walls
+  if (headX < 0) {
+    gameOver = true;
+  } else if (headX === tileCount) {
+    gameOver = true;
+  } else if (headY < 0) {
+    gameOver = true;
+  } else if (headY === tileCount) {
+    gameOver = true;
+  }
+
+  for (let i = 0; i < snakeParts.length; i++) {
+    let part = snakeParts[i];
+    if (part.x === headX && part.y === headY) {
+      gameOver = true;
+      break;
     }
-    ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-  })
-
-  ctx.fillStyle = '#E1B48D '
-  foods.forEach(({ x, y }) => {
-    ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-  })
-
-  ctx.fillStyle = '#000000'
-  ctx.font = '20px Arial'
-  ctx.fillText(`Pontszám: ${score}`, canvas.width / 2, CELL_SIZE / 2)
+  }
 
   if (gameOver) {
-    ctx.fillStyle = 'red'
-    ctx.font = '60px Arial'
-    ctx.fillText('JÁTÉK VÉGE!', canvas.width / 2, canvas.height / 2)
+    ctx.fillStyle = "white";
+    ctx.font = "50px Verdana";
 
-    ctx.fillStyle = 'black'
-    ctx.font = '15px Arial'
-    ctx.fillText('Nyomj egy SPACE-t az újraindításhoz', canvas.width / 2, canvas.height / 2 + 40)
+    if (gameOver) {
+      ctx.fillStyle = "white";
+      ctx.font = "50px Verdana";
+
+      var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+      gradient.addColorStop("0", " magenta");
+      gradient.addColorStop("0.5", "blue");
+      gradient.addColorStop("1.0", "red");
+      // Fill with gradient
+      ctx.fillStyle = gradient;
+
+      ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+    }
+
+    ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+  }
+
+  return gameOver;
+}
+
+function drawScore() {
+  ctx.fillStyle = "white";
+  ctx.font = "10px Verdana";
+  ctx.fillText("Score " + score, canvas.width - 50, 10);
+}
+
+function clearScreen() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawSnake() {
+  ctx.fillStyle = "green";
+  for (let i = 0; i < snakeParts.length; i++) {
+    let part = snakeParts[i];
+    ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+  }
+
+  snakeParts.push(new SnakePart(headX, headY)); //put an item at the end of the list next to the head
+  while (snakeParts.length > tailLength) {
+    snakeParts.shift(); // remove the furthet item from the snake parts if have more than our tail size.
+  }
+
+  ctx.fillStyle = "orange";
+  ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+}
+
+function changeSnakePosition() {
+  headX = headX + xVelocity;
+  headY = headY + yVelocity;
+}
+
+function drawApple() {
+  ctx.fillStyle = "red";
+  ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize);
+}
+
+function checkAppleCollision() {
+  if (appleX === headX && appleY == headY) {
+    appleX = Math.floor(Math.random() * tileCount);
+    appleY = Math.floor(Math.random() * tileCount);
+    tailLength++;
+    score++;
+    gulpSound.play();
   }
 }
-//
-let lastLoopTime = Date.now()
-function gameLoop () {
-  const now = Date.now()
-  const delta = now - lastLoopTime
-  lastLoopTime = now
 
-  update(delta)
-  render()
+document.body.addEventListener("keydown", keyDown);
 
-  window.requestAnimationFrame(gameLoop)
+function keyDown(event) {
+  //up
+  if (event.keyCode == 38 || event.keyCode == 87) {
+    //87 is w
+    if (inputsYVelocity == 1) return;
+    inputsYVelocity = -1;
+    inputsXVelocity = 0;
+  }
+
+  //down
+  if (event.keyCode == 40 || event.keyCode == 83) {
+    // 83 is s
+    if (inputsYVelocity == -1) return;
+    inputsYVelocity = 1;
+    inputsXVelocity = 0;
+  }
+
+  //left
+  if (event.keyCode == 37 || event.keyCode == 65) {
+    // 65 is a
+    if (inputsXVelocity == 1) return;
+    inputsYVelocity = 0;
+    inputsXVelocity = -1;
+  }
+
+  //right
+  if (event.keyCode == 39 || event.keyCode == 68) {
+    //68 is d
+    if (inputsXVelocity == -1) return;
+    inputsYVelocity = 0;
+    inputsXVelocity = 1;
+  }
 }
-reset()
-gameLoop()
 
-window.addEventListener('keydown', (event) => {
-  input[event.key] = true
-})
-window.addEventListener('keyup', (event) => {
-  input[event.key] = false
-})
+drawGame();
